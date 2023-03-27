99cb1c19274c5ffd371d6146222fde3d26fcf58f
yantlord <99481791+yantlord@users.noreply.github.com>
2023-03-27 16:25:54 -0700

    Update run.js

--- js/run.js
+++ js/run.js
@@ -1,30 +1,29 @@
 window.addEventListener("load", () => {
-    alert("weiner ")
-    
+
     let socket = new WebSocket("wss://127.0.0.1:4201");
 
     socket.onopen = function(e) {
-        alert("[open] Connection established");
-        alert("Sending to server");
+        console.log("[open] Connection established");
+        console.log("Sending to server");
         socket.send("My name is John");
     };
 
     socket.onmessage = function(event) {
-        alert(`[message] Data received from server: ${event.data}`);
+        console.log(`[message] Data received from server: ${event.data}`);
     };
 
     socket.onclose = function(event) {
         if (event.wasClean) {
-            alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
         } else {
             // e.g. server process killed or network down
             // event.code is usually 1006 in this case
-            alert('[close] Connection died');
+            console.log('[close] Connection died');
         }
     };
 
     socket.onerror = function(error) {
-        alert(`[error]`);
+        console.log(`[error]`);
     };
     
 })
